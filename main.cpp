bddc4a05b33f34e0ae2b654d9af9dd0f44854232
Emanuel Sancho <>
2021-05-24 08:40:31 +0000

    Anotaciones a todo el programa

--- main.cpp
+++ main.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-#include <string>
+#include <iostream>//biblioteca de variables y comandos
+#include <string>//biblioteca de variables y comandos
 using namespace std;
 
-int main()
+int main()//inicializacion del programa
 {
 
-int i, tiempo[31],sumaluz=0, sumacamara=0;
-string salir, nombre[31], tipo[31], Luz="1", Camara="2";
-double consumo[31];
+int i, tiempo[31],sumaluz=0, sumacamara=0; //declaración de variables
+string salir, nombre[31], tipo[31], Luz="1", Camara="2";//declaración de variables tipo texto
+double consumo[31]; //declaración de variable tipo numeros reales
 
-cout<<"\nPrograma para introducir inventario de dispositivos(max=30)\n"<<endl;
-while(salir!="2"){
-  i++;
-  cout<<"\nIngrese nombre del dispositivo\n";
-  cin.ignore();
-  getline(cin,nombre[i]);
-  cout<<"\nIngrese el tipo de artículo\n        (1)-LUZ\n        (2)-CÁMARA"<<endl;
-  cin>>tipo[i];
-  cout<<"\nIngrese el consumo en watts del artículo"<<endl;
-  cin>>consumo[i];
-  cout<<"\nDesea continuar ingresando los dispositivos?\n        (1)-Para continuar\n        (2)-Para finalizar"<<endl;
-  cin>>salir;
-}
+cout<<"\nPrograma para introducir inventario de dispositivos(max=30)\n"<<endl; // muestra en pantalla el texto
+while(salir!="2"){//ciclo de repeticion del programa
+  i++;//suma 1 a la variable i
+  cout<<"\nIngrese nombre del dispositivo\n";//muestra en pantalla el texto
+  cin.ignore();//para ignorar el comando final que crea el bucle
+  getline(cin,nombre[i]);//comando para dar lectura a todo el texto
+  cout<<"\nIngrese el tipo de artículo\n        (1)-LUZ\n        (2)-CÁMARA"<<endl;//muestra en pantalla el texto
+  cin>>tipo[i];//ingreso de información
+  cout<<"\nIngrese el consumo en watts del artículo"<<endl;//muestra en pantalla el texto
+  cin>>consumo[i];//ingreso de información
+  cout<<"\nDesea continuar ingresando los dispositivos?\n        (1)-Para continuar\n        (2)-Para finalizar"<<endl;//muestra en pantalla el texto
+  cin>>salir;//Comando para enciclar el while
+}//fin del while
 
-for(i=1;i<31;i++){
-if(nombre[i]!=""){
-cout<<"\nIngrese el tiempo de funcionamiento de " <<nombre[i]<<" horas\n";
-cin>>tiempo[i];
-}}
-for(i=1;i<31;i++){
-if(tipo[i] == Luz){
-sumaluz= tiempo[i]*consumo[i] + sumaluz;}
-if(tipo[i] == Camara){
-sumacamara= tiempo[i]*consumo[i] + sumacamara;}
-}
-cout<<"\n""El total consumido por Cámaras es: " <<sumacamara<<" Watts-hora\n"<<endl;
-cout<<"El total consumido por Luz es: " <<sumaluz<<" Watts-hora\n"<<endl;
-return 0;
-}
+for(i=1;i<31;i++){//bucle para llenar el tiempo
+if(nombre[i]!=""){//Condicion de entrada
+cout<<"\nIngrese el tiempo de funcionamiento de " <<nombre[i]<<" horas\n";//muestra en pantalla el texto
+cin>>tiempo[i];//solicitud del tiempo
+}}//finalizar ciclo
+for(i=1;i<31;i++){//bucle para realizar las operaciones 
+if(tipo[i] == Luz){//condion para averiguar si es Luz
+sumaluz= tiempo[i]*consumo[i] + sumaluz;}//operacion de potencia por el tiempo
+if(tipo[i] == Camara){//Condicion para averiguar la Camara
+sumacamara= tiempo[i]*consumo[i] + sumacamara;}//operacion
+}//fin del bucle
+cout<<"\n""El total consumido por Cámaras es: " <<sumacamara<<" Watts-hora\n"<<endl;//muestra en pantalla el texto y la totalidad
+cout<<"El total consumido por Luz es: " <<sumaluz<<" Watts-hora\n"<<endl;//muestra en pantalla el texto y la totalidad
+return 0;//regresa valores a 0
+}//fin del programa
 
